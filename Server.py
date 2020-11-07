dc1a68936a1ade21cf0c352600583d2f037877cf
Ross Creaven <rosscreaven@gmail.com>
2020-11-07 16:43:40 +0000

    Added simple message decoder

--- Server.py
+++ Server.py
@@ -2,39 +2,34 @@ import socket
 import urllib.request
 import Get_IP
  
+def decode_instruction(msg):
+	if "FWD" in msg:
+		print("moving forward")
+	elif "BCK" in msg:
+		print("moving back")
+	elif "LEFT" in msg:
+		print("moving left")
+	elif "RIGHT" in msg:
+		print("moving right")
+
+
 localIP = Get_IP.get_ip()
 print(localIP)
 
 localPort   = 20001
-
 bufferSize  = 1024
 
  
 
 msgFromServer       = "Hello UDP Client"
-
 bytesToSend         = str.encode(msgFromServer)
-
- 
-
 # Create a datagram socket
-
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
- 
-
 # Bind to address and ip
-
 UDPServerSocket.bind((localIP, localPort))
-
- 
-
 print("UDP server up and listening")
 
- 
-
 # Listen for incoming datagrams
-
 while(True):
 
     bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
@@ -46,8 +41,8 @@ while(True):
     clientMsg = "Message from Client: '{}' ".format(message)
     clientIP  = "Client IP Address:{}".format(address)
 
-    if message == "one":
-         print("true")
+    decode_instruction(message.upper())
+    
     print(clientMsg)
     print(clientIP)
     # Sending a reply to client
