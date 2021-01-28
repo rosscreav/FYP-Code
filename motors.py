bd60b2f035a15fe3688e205edf2fc25123395c71
rosscreav <rosscreaven@gmail.com>
2021-01-28 18:01:12 +0000

    Added full motor direction test

--- motors.py
+++ motors.py
@@ -3,14 +3,38 @@ import time
 
 k = MotorKit()
 
-k.motor1.throttle = -0.5
-k.motor2.throttle = -0.5
-k.motor3.throttle = 0.5
-k.motor4.throttle = 0.5
+def forward(d=1):
+	k.motor1.throttle = -0.5*d
+	k.motor2.throttle = -0.5*d
+	k.motor3.throttle = 0.5*d
+	k.motor4.throttle = 0.5*d
 
-time.sleep(4)
+def back():
+	forward(-1)
 
-k.motor1.throttle = 0
-k.motor2.throttle = 0
-k.motor3.throttle = 0
-k.motor4.throttle = 0
+def right(d=1):
+	k.motor1.throttle = -0.5*d
+	k.motor2.throttle = 0.5*d
+	k.motor3.throttle = -0.5*d
+	k.motor4.throttle = 0.5*d
+	time.sleep(1)
+	
+
+def left():
+	right(-1)
+
+def stop():
+	k.motor1.throttle = 0
+	k.motor2.throttle = 0
+	k.motor3.throttle = 0
+	k.motor4.throttle = 0
+
+if __name__ == "__main__":
+	forward()
+	sleep(5)
+	back()
+	sleep(5)
+	right()
+	sleep(5)
+	left()
+	sleep(5)
