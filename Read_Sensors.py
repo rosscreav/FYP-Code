e49f21137d8463997310bdaad30ecc8aeacf146d
Ross <56259013+rosscreav@users.noreply.github.com>
2021-02-02 21:56:35 +0000

    Updated Sensor sending script

--- Read_Sensors.py
+++ Read_Sensors.py
@@ -1,11 +1,15 @@
 ##Imports
 import RPi.GPIO as GPIO
 import time
+import serial
 
-GPIO.cleanup()
+ser = serial.Serial("/dev/ttyAMA0", 115200)
 
-TRIG1 = 23
-ECHO2 = 24
+trig_l = 23
+echo_l = 24
+
+trig_r = 23
+echo_r = 24
 
 GPIO.setmode(GPIO.BCM)
 
@@ -14,14 +18,14 @@ def read_ultrasound(trig, echo)
 	GPIO.setup(echo,GPIO.IN)
 	GPIO.output(trig, False)
 	time.sleep(1)
-	GPIO.output(TRIG, True)
+	GPIO.output(trig, True)
 	time.sleep(0.00001)
-	GPIO.output(TRIG, False)
+	GPIO.output(trig, False)
 	##When pulse start
-	while GPIO.input(ECHO)==0:
+	while GPIO.input(echo)==0:
 		pulse_start = time.time()
 	##When pulse end
-	while GPIO.input(ECHO)==1:
+	while GPIO.input(echo)==1:
 		pulse_end = time.time()
 
 	##Length Calculation
@@ -30,5 +34,22 @@ def read_ultrasound(trig, echo)
 	distance = round(distance,2)
 	return distance
 
+def getTFminiData():
+    while True:
+        count = ser.in_waiting
+        if count > 8:
+            recv = ser.read(9)
+            ser.reset_input_buffer()
+            if recv[0] == 'Y' and recv[1] == 'Y': # 0x59 is 'Y'
+                low = int(recv[2].encode('hex'), 16)
+                high = int(recv[3].encode('hex'), 16)
+                distance = low + high * 256
+                return distance
 
+def read_sensor_data():
+	ultrasound_l = read_ultrasound(trig_l,echo_l)
+	ultrasound_r = read_ultrasound(trig_r,echo_r)
+	lidar = getTFminiData()
+	GPIO.cleanup()
+	return {"left" : ultrasound_l,"right" : ultrasound_r, "front" : lidar}
 
