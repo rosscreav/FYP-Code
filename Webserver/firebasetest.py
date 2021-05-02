7b2026c32d2ec3609ac0e3b73baf86a6728a9ab9
rosscreav <rosscreaven@gmail.com>
2021-05-02 23:21:11 +0100

    Added room map creator

--- Webserver/firebasetest.py
+++ Webserver/firebasetest.py
@@ -1,19 +1,23 @@
 from firebase import firebase
+import plot_room as p 
+
 
 def get_most_recent_data():
     fb = firebase.FirebaseApplication("https://fyp-database-7e287-default-rtdb.europe-west1.firebasedatabase.app/",None)
 
-    most_recent_entries = []
+    MapData = []
 
     db_entries = fb.get('/MapData/',None)
     db_entries = list(db_entries.values())
     db_entries.reverse()
-    for entry in db_entries:
-        most_recent_entries.append(entry)
-        if len(most_recent_entries) > 60:
-            break
+    for entry in db_entries[0]:
+        MapData.append(entry)
+        
 
-    return most_recent_entries
+    return MapData
 
-print(get_most_recent_data())
+if __name__ == '__main__':
+	MapData = get_most_recent_data()
+    p.plot(MapData)
+
 
